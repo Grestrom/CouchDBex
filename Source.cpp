ae1f61db102664fd67d3500edf6144b2ac6026bb
Grestrom <38920762+Grestrom@users.noreply.github.com>
2018-05-03 00:33:43 +0300

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,2 @@
+#include <C:\Users\Антон\Documents\Visual Studio 2015\Projects\CouchDBex\packages\curl.7.30.0.2\build\native\include\curl\curl.h>
+#include <C:\Users\Антон\Documents\Visual Studio 2015\Projects\CouchDBex\packages\curl.7.30.0.2\build\native\lib\v110\x64\Release\dynamic\libcurl.lib>
